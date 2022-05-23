739358acdcec18aacec1c871ba3c039f3bdf18f8
Nikolai Schunk <nikolai.schunk@gmail.com>
2022-05-23 16:30:31 +0200

    Tried to add Theme to Bulma

---
+++ components/Main.js
@@ -0,0 +1,10 @@
+const Main = () => {
+    return ( 
+        <div>
+        <button className="button is-primary">abcde</button>
+        <button className="button is-link">aaaaa</button>
+        </div>
+        );
+}
+ 
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar";
-import "../styles/pages/Home.module.scss";
+import Main from "../components/Main";
 
 export default function Home() {
   return (
     <div className="main-content">
       <Navbar />
+      <div className="container">
+        <Main />
+      </div>
     </div>
+
   );
 }
 //write sfc for template export
