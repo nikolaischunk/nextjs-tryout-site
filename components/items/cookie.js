895765a1dc0a63b737c9447d2604b306bbf3fd81
Nikolai Schunk <nikolai.schunk@gmail.com>
2022-05-30 16:26:57 +0200

    Added Cookie card

---
+++ components/items/cookie.js
@@ -0,0 +1,11 @@
+const cookie = () => {
+    return (<div class="cookie">
+        <div class="notification is-primary">
+            <button class="delete"></button>{/*Add Closing functionality */}
+            Primar lorem ipsum dolor sit amet, consectetur
+            adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur.
+        </div>
+    </div>);
+}
+
+export default cookie;
