125d204400813a137ba2dc9b667c97a2abd039d9
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-06-03 17:36:18 +0200

    Timeout for the cookie until used again

--- pages/api/cookie.js
+++ pages/api/cookie.js
@@ -1,16 +1,16 @@
-import cookieCutter from 'cookie-cutter'
-// want some?
+// import cookieCutter from 'cookie-cutter'
+// // want some?
 
-export default ({ get: getCookie, post: setCookie }) => {
+// export default ({ get: getCookie, post: setCookie }) => {
     
-}
+// }
 
-function getCookie(cookieName, req, res) {
-    let cookie = cookieCutter.get(cookieName)
-    return cookie;
-}
+// function getCookie(cookieName, req, res) {
+//     let cookie = cookieCutter.get(cookieName)
+//     return cookie;
+// }
 
-function getByQuestion(question) {
-    return faqs.find(qa => qa.question.toString() === question.toString());
-}
+// function getByQuestion(question) {
+//     return faqs.find(qa => qa.question.toString() === question.toString());
+// }
 
