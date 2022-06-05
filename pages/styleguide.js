101f738d4c854e74de9d57fffdc06bf40e4d27a1
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-06-06 00:40:51 +0200

    Changed style

--- pages/styleguide.js
+++ pages/styleguide.js
@@ -2,10 +2,12 @@ import Fonts from "../components/styleguide/Fonts";
 import Colors from "../components/styleguide/Colors";
 import Buttons from "../components/styleguide/Buttons";
 import Header from "../components/styleguide/Header";
+import Heading from "../components/items/heading";
 const styleguide = () => {
     return (
         <div className="styleguide">
             <div className="container">
+                <Heading heading="Styleguide" subheading="This is an Overview over different Style aspects that get implemented on this Website." divider=" " />
                 <Fonts />
                 <Colors />
                 <Buttons />
