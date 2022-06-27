d6dbc18eb77ed9169a9646eb0bf63c0a3f71ff0b
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-06-27 11:49:52 +0200

    Started with a custom typo class

---
+++ components/items/typography.js
@@ -0,0 +1,7 @@
+function Title(text) {
+  return <h1 className="is-title has-text-weight-bold">{text}</h1>;
+}
+function SubTitle(text) {
+    return <p className="is-sub-title has-text-weight-light">{text}</p>;
+}
+export { Title, SubTitle };
