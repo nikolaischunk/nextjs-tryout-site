d40d11b8bed634820dfa7664f120634c188759b1
Chillihero <nikolai.schunk@gmail.com>
2022-07-04 14:46:51 +0200

    Added Generic Button Class

---
+++ components/items/button.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Button = ({ type, isOutlined, label, link }) => {
+  let button_class = "button is-large is-fullwidth is-family-title";
+  button_class += type === "link" ? " is-link" : " is-primary";
+  button_class += isOutlined ? " is-outlined" : "";
+
+  console.log(link);
+  console.log(button_class);
+  return (
+    <div class="custom_button column">
+      <Link href={link}>
+        <button className={button_class}>{label}</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Button;
