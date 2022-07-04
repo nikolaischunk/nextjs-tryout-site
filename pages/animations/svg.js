949750c79c3da2df1111e56d2997a3ce99c0fae6
nikolaischunk <58025175+nikolaischunk@users.noreply.github.com>
2022-07-04 10:46:22 +0200

    tried implementing a svg animation (and failed)

---
+++ pages/animations/svg.js
@@ -0,0 +1,10 @@
+import SVGAnimations from "../../components/animations/SVGAnimations";
+const svg = () => {
+  return (
+    <div className="container">
+      <SVGAnimations />
+    </div>
+  );
+};
+
+export default svg;
